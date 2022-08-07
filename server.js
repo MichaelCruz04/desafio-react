2d3c6ee2f8b5ae9d63f2fe366370ec4e632761ac
MichaelCruz <michaelmdacruz@gmail.com>
2022-08-07 01:25:37 -0300

    Fix require path name

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const resolve = require("path");
+const path = require("path");
 
 const app = express();
 
-app.use("/", express.static(resolve(__dirname, "./build")));
+app.use("/", express.static(path.resolve(__dirname, "./build")));
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
